8e5b53bedb5ab5547518d962d09725724203fa26
Ben_G <ben.goodwin.work1@gmail.com>
2023-02-03 10:54:10 +0000

    updated add channel functionality

--- src/comps/CreateChannel.jsx
+++ src/comps/CreateChannel.jsx
@@ -1,11 +1,11 @@
+
 import React, { useState } from 'react'
 import { useChatContext } from 'stream-chat-react';
 import {UserList} from './';
-import { CloseCreateChannel} from './';
+// import { CloseCreateChannel} from './';
 
 
 const ChannelNameInput = ({channelName = '', setChannelName}) => {
-
   const handleChange = (e) => {
     e.preventDefault();
 
@@ -23,10 +23,29 @@ const ChannelNameInput = ({channelName = '', setChannelName}) => {
 }
 
 const CreateChannel = ({createType, setIsCreating}) => {
-
+  const {client, setActiveChannel} = useChatContext();
+  const [selectedUsers, setSelectedUsers] = useState([client.user.id || '']);
   const [channelName, setChannelName] = useState('');
 
+  const createChannel = async (e) => {
+    e.preventDefault();
 
+    try{
+      const newChannel = await client.channel(createType, channelName, {
+        name: channelName, members: selectedUsers
+      });
+
+      // await newChannel.watch();
+
+      setChannelName('');
+      setIsCreating(false);
+      setSelectedUsers([client.userID]);
+      setActiveChannel(newChannel);
+    } 
+    catch(error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className='create-channel__container'>
@@ -34,8 +53,11 @@ const CreateChannel = ({createType, setIsCreating}) => {
         <p>{createType === 'team' ? 'Create a New Channel' : 'Send a Direct Message'}</p>
         {/* <CloseCreateChannel setIsCreating={setIsCreating}/> */}
       </div>
-      {createType === 'team' && <ChannelNameInput name={channelName} setChannelName={setChannelName} />}
-      <UserList />
+      {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />}
+      <UserList setSelectedUsers={setSelectedUsers}/>
+      <div className="create-channel__button-wrapper" onClick={createChannel}>
+        <p>{createType === 'team' ? 'Create Channel' : 'Create Message Group'}</p>
+      </div>
     </div>
   )
 }
--- src/comps/UserList.jsx
+++ src/comps/UserList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Avatar, useChatChannel, useChatContext } from "stream-chat-react";
+import { Avatar, useChatContext } from "stream-chat-react";
+import { InviteIcon } from "../assets/InviteIcon";
 //
 
 const ListContainer = ({ children }) => {
@@ -15,20 +16,35 @@ const ListContainer = ({ children }) => {
   );
 };
 
-const UserItem = ({ user }) => {
+const UserItem = ({ user, setSelectedUsers }) => {
+  const [selected, setSelected] = useState(false);
+
+  const handleSelect = () => {
+    if (selected) {
+      setSelectedUsers((prevUsers) =>
+        prevUsers.filter((prevUser) => prevUser !== user.id)
+      );
+    } else {
+      setSelectedUsers((prevUsers) => [...prevUsers, user.id]);
+    }
+    setSelected((prevselected) => !prevselected);
+  };
   return (
-    <div className="user-item__wrapper">
+    <div className="user-item__wrapper" onClick={handleSelect}>
       <div className="user-item__name-wrapper">
-        <Avatar image={user.image} name={user.fullName} size={32} />
+        <Avatar image={user.image} name={user.fullName || user.id} size={32} />
+        <p className="user-item__name">{user.fullName || user.id}</p>
       </div>
+      {selected ? <InviteIcon /> : <div className="user-item__invite-empty" />}
     </div>
   );
 };
 
-const UserList = () => {
-  const [users, setUsers] = useState(['a', 'b', 'c']);
+const UserList = ({setSelectedUsers}) => {
+  const [users, setUsers] = useState([]);
   const [loading, setIsLoading] = useState(false);
   const [listEmpty, setListEmpty] = useState(false);
+  const [error, setError] = useState(false);
   const { client } = useChatContext();
   useEffect(() => {
     const getUsers = async () => {
@@ -54,22 +70,40 @@ const UserList = () => {
         }
       } catch (error) {
         console.log(error);
+        setError(true);
       }
       setIsLoading(false);
     };
     if (client) getUsers();
   }, []);
 
-  const userMap = users.map((user, i) => (
-    <UserItem index={i} key={user.id} user={user}  />  
-  ))
+  if (error) {
+    return (
+      <div className="user-list__message">
+        Error loading, please refresh and try again
+      </div>
+    );
+  }
+  if (listEmpty) {
+    return <div className="user-list__message">No Users Found</div>;
+  }
 
+  const userMap = users.map((user, i) => (
+    <UserItem
+      setSelectedUsers={setSelectedUsers}
+      index={i}
+      key={user.id}
+      user={user}
+    />
+  ));
 
   return (
     <ListContainer>
-      {loading ? 
-        <div className="user-list__message"> Loading Users... </div>
-       : userMap}
+      {loading ? (
+        <div className="user-list__message" > Loading Users... </div>
+      ) : (
+        userMap
+      )}
     </ListContainer>
   );
 };
